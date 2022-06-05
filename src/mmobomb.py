8d241caa80b632c0556e83a14939e615154bce24
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-06-05 17:38:19 +0600

    Create mmobomb.py

---
+++ src/mmobomb.py
@@ -0,0 +1,57 @@
+from requests import get
+
+
+class MMOBomb:
+	def __init__(self):
+		self.api = "https://www.mmobomb.com/api1"
+		self.headers = {
+			"User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
+		}
+	
+	def get_games(self):
+		return get(f"{self.api}/games", headers=self.headers).json()
+	
+	def get_games_by_platform(self, platform: str = "pc"):
+		return get(
+			f"{self.api}/games?platform={platform}",
+			headers=self.headers).json()
+	
+	def get_games_by_category(self, category: str):
+		return get(
+			f"{self.api}/games?category={category}",
+			headers=self.headers).json()
+	
+	def get_games_by_tag(self, tag: str):
+		return get(f"{self.api}/games?tag={tag}", headers=self.headers).json()
+	
+	def sort_games(self, sort: str):
+		return get(
+			f"{self.api}/games?sort-by={sort}", headers=self.headers).json()
+	
+	def get_games_by_all(
+			self,
+			platform: str = "browser",
+			category: str = "mmorpg",
+			sort: str = "release-date"):
+		return get(
+			f"{self.api}/games?platform={platform}&category={category}&sort-by={sort}",
+			headers=self.headers).json()
+	
+	def filter_games(
+			self,
+			platform: str = "pc",
+			tag: str = "3d.mmorpg.fantasy.pvp"):
+		return get(
+			f"{self.api}/filter?tag={tag}&platform={platform}",
+			headers=self.headers).json()
+		
+	def get_game_details(self, game_id: int):
+		return get(
+			f"{self.api}/game?id={game_id}",
+			headers=self.headers).json()
+	
+	def get_mmo_giveaways(self):
+		return get(f"{self.api}/giveaways", headers=self.headers).json()
+	
+	def get_latest_mmo_news(self):
+		return get(f"{self.api}/latestnews", headers=self.headers).json()
